df63b97f6cb9ec8f0f60dba0bb42e63c6e30422d
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-07-23 12:36:27 +0000

    Add: word to uppercase

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 import requests
 
-word = requests.get("https://random-word-api.herokuapp.com/word?lang=en&length=6&number=5")
-print(word.text)
+get_word = requests.get("https://random-word-api.herokuapp.com/word?lang=en&length=6&number=1")
+
+word = get_word.text.upper()
+print(word)
+
+guess_this_word = ()
+
