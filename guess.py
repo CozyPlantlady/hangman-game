7276dd13a8a09d5be2ffe0393cff32f9d62db050
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-07-25 08:23:37 +0000

    Change: guess.py, separated functions to get new return values

--- guess.py
+++ guess.py
@@ -1,30 +1,51 @@
 from word import validate_word
 
+
 guessed_letters = []
 word = validate_word()
 
 
 def user_guess():
     """
-    Getting the users answer and adding it to guessed letters list,
-    then returning updated guessed letters list
+    Getting the users answer
     """
-    print(word)
-    guessed_letter = input("Guess a letter:").upper()
-    for letter in word:
-        if guessed_letter in letter:
-            print(letter, end=" ")
-        elif guessed_letter in guessed_letters:
-            print(guessed_letters, end=" ")
-            print("You already guessed {guessed_letter}. Try again.")
-        else:
-            print("_", end=" ")
-    guessed_letters.append(guessed_letter)
+    input_guess = input("Guess a letter:").upper()
+    return input_guess
+
+
+def collect_guessed_letters():
+    """
+    Collects letters user has guessed
+    """
+    ### Needs to get input_guess value correctly
+    guessed_letters.append(input_guess)
     print(f"\n Guessed letters:{guessed_letters}")
+    print(guessed_letters)
     return guessed_letters
 
-#guess = 0
-#
-#while guess <= 10:
-#    user_guess()
-#    guess += 1
+
+def print_word():
+    """
+    Takes values from user_guess and collect_guessed_letters
+    to decide which letters to print.
+    """
+    user_guess()
+    collect_guessed_letters()
+    for letter in word:
+        if input_guess in letter:
+            print(letter, end=" ")
+        elif input_guess in guessed_letters:
+            print("You already guessed {input_guess}. Try again.")
+        elif guessed_letters in letter:
+            print(letter, end=" ")
+        else:
+            print("_", end=" ")
+
+
+guess = 0
+
+
+while guess <= 10:
+    guess += 1
+    user_guess()
+    collect_guessed_letters()
