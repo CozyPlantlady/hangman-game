735636a68c5b86c3efae492709bb41a831038af4
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-07-28 12:40:26 +0000

    Change: order of the codelines in loop_this. Game is now working!

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ from word import validate_word
 game_on = True
 correct_answers = 0
 wrong_answers = 0
+guessed_letters = []
 
 
 def game_loop():
@@ -25,7 +26,7 @@ def game_loop():
             print("Congratulations! You WON!")
         if wrong_answers == 6:
             print("Sorry, you lost :(")
-        print("Ooops")
+        print("Game has ended.")    
 
 
 def loop_this():
@@ -35,32 +36,31 @@ def loop_this():
     """
     global correct_answers
     global wrong_answers
-    guessed_letters = []
+    global guessed_letters
+    global game_on
     word = validate_word()
     while wrong_answers < 6 and correct_answers < len(word):
         input_guess = user_guess()
+        for letter in word:
+            if input_guess in letter:
+                print(letter, end=" ")
+                guessed_letters.append(input_guess)
+                correct_answers += 1
+            elif letter in guessed_letters and word:
+                print(letter, end=" ")
+            else:
+                print("_", end=" ")
         if input_guess in guessed_letters:
-            print(f"You already guessed {input_guess}. Try again.")
+            print(f"You already guessed {input_guess}. Try again.\n")
         elif input_guess not in word:
-            print(f"Sorry, {input_guess} is not in this word.")
+            print(f"Sorry, {input_guess} is not in this word.\n")
             guessed_letters.append(input_guess)
             wrong_answers += 1
             print_hangman(wrong_answers)
-            print(f"\n Guessed letters:{guessed_letters}")
+            print(f"\n You have already guessed:{guessed_letters}\n")
             return guessed_letters, wrong_answers
-        elif input_guess in word:
-            for letter in word:
-                if input_guess in letter:
-                    print(letter, end=" ")
-                    guessed_letters.append(input_guess)
-                    correct_answers += 1
-                    return correct_answers
-                if letter in guessed_letters and word:
-                    print(letter, end=" ")
-                else:
-                    print("_", end=" ")
-                    print(f"\n Guessed letters:{guessed_letters}")
-                    return guessed_letters   
-    global game_on
+        #elif input_guess in word:
+            
+        print(f"\n Guessed letters:{guessed_letters}\n")
     game_on = False
     return game_on
