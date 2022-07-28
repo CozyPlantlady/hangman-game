197461831da8701cb0a7d3b9033b45ba90ed9f21
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-07-28 11:16:12 +0000

    Add: Guess.py fix that user can give only a single letter

--- guess.py
+++ guess.py
@@ -6,5 +6,7 @@ def user_guess():
     if input_guess.isalpha() is False:
         print(f"{input_guess} is not an alphabet.")
         user_guess()
+    if len(input_guess) != 1:
+        print("Please give a single letter.")
     else:
         return input_guess
