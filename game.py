7e23680c56cfc8d01b010203498371fdf1d99266
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-07-28 12:56:45 +0000

    Change: name of validate_word to get_word

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 from logo import print_logo
 from hangman import print_hangman
 from guess import user_guess
-from word import validate_word
+from word import get_word
 
 game_on = True
 correct_answers = 0
@@ -13,7 +13,7 @@ def game_loop():
     """
     Loops until user loses or wins
     """
-    word = validate_word()
+    word = get_word()
     global correct_answers
     global wrong_answers
     print_logo()
@@ -26,7 +26,7 @@ def game_loop():
             print("Congratulations! You WON!")
         if wrong_answers == 6:
             print("Sorry, you lost :(")
-        print("Game has ended.")    
+        print("Game has ended.")
 
 
 def loop_this():
@@ -38,9 +38,18 @@ def loop_this():
     global wrong_answers
     global guessed_letters
     global game_on
-    word = validate_word()
+    word = get_word()
     while wrong_answers < 6 and correct_answers < len(word):
         input_guess = user_guess()
+        if input_guess in guessed_letters:
+            print(f"You already guessed {input_guess}. Try again.\n")
+        elif input_guess not in word:
+            print(f"Sorry, {input_guess} is not in this word.\n")
+            guessed_letters.append(input_guess)
+            wrong_answers += 1
+            print_hangman(wrong_answers)
+            print(f"You have already guessed:\n{guessed_letters}\n")
+            return guessed_letters, wrong_answers
         for letter in word:
             if input_guess in letter:
                 print(letter, end=" ")
@@ -50,17 +59,6 @@ def loop_this():
                 print(letter, end=" ")
             else:
                 print("_", end=" ")
-        if input_guess in guessed_letters:
-            print(f"You already guessed {input_guess}. Try again.\n")
-        elif input_guess not in word:
-            print(f"Sorry, {input_guess} is not in this word.\n")
-            guessed_letters.append(input_guess)
-            wrong_answers += 1
-            print_hangman(wrong_answers)
-            print(f"\n You have already guessed:{guessed_letters}\n")
-            return guessed_letters, wrong_answers
-        #elif input_guess in word:
-            
         print(f"\n Guessed letters:{guessed_letters}\n")
     game_on = False
     return game_on
--- word.py
+++ word.py
@@ -1,13 +1,13 @@
 import requests
 
 
-get_word = requests.get("https://random-word-api.herokuapp.com/word?lang=en&length=6&number=1")
+request = requests.get("https://random-word-api.herokuapp.com/word?lang=en&length=6&number=1")
 
 
-def validate_word():
+def get_word():
     """
     Gets the random word and removes symbols
     """
-    new_word = get_word.text.upper()
+    new_word = request.text.upper()
     word = new_word.translate({ord(new_word): None for new_word in '[]"'})
     return word
