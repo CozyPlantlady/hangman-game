a28cfe232c11e922f5f505ae231166b7607f3f35
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-08-03 11:29:53 +0000

    Change global variables to uppercase

--- game.py
+++ game.py
@@ -3,10 +3,10 @@ from utils.hangman import print_hangman, print_hangman_win
 from guess import user_guess
 from utils.word import get_word
 
-game_on = True
-correct_answers = 0
-wrong_answers = 0
-guessed_letters = []
+GAME_ON = True
+CORRECT_ANSWERS = 0
+WRONG_ANSWERS = 0
+GUESSED_LETTERS = []
 
 
 def game_loop():
@@ -14,14 +14,14 @@ def game_loop():
     Loops until user loses or wins
     """
     word = get_word()
-    global correct_answers
-    global wrong_answers
+    global CORRECT_ANSWERS
+    global WRONG_ANSWERS
     print_logo()
-    print_hangman(wrong_answers)
-    while game_on is True:
+    print_hangman(WRONG_ANSWERS)
+    while GAME_ON is True:
         loop_this()
-    if game_on is not True:
-        if wrong_answers == 6:
+    if GAME_ON is not True:
+        if WRONG_ANSWERS == 6:
             print(f"Sorry, you lost :( The word was {word}")
         else:
             print_hangman_win()
@@ -34,34 +34,33 @@ def loop_this():
     The loop that takes users answer, checks if it's correct or wrong,
     and gives points.
     """
-    global correct_answers
-    global wrong_answers
-    global guessed_letters
-    global game_on
+    global CORRECT_ANSWERS
+    global WRONG_ANSWERS
+    global GUESSED_LETTERS
+    global GAME_ON
     word = get_word()
-    while wrong_answers < 6 and correct_answers < len(word):
+    while WRONG_ANSWERS < 6 and CORRECT_ANSWERS < len(word):
         input_guess = user_guess()
-        if input_guess in guessed_letters:
+        if input_guess in GUESSED_LETTERS:
             print(f"You already guessed {input_guess}. Try again.\n")
         elif input_guess not in word:
             print(f"Sorry, {input_guess} is not in this word.")
-            guessed_letters.append(input_guess)
-            wrong_answers += 1
-            print_hangman(wrong_answers)
-            print(f"Used letters:\n{guessed_letters}\n")
-            return guessed_letters, wrong_answers
+            GUESSED_LETTERS.append(input_guess)
+            WRONG_ANSWERS += 1
+            print_hangman(WRONG_ANSWERS)
+            print(f"Used letters:\n{GUESSED_LETTERS}\n")
+            return GUESSED_LETTERS, WRONG_ANSWERS
         elif input_guess in word:
             print(f"Correct! {input_guess} is in the word!\n")
         for letter in word:
             if input_guess in letter:
                 print(letter, end=" ")
-                guessed_letters.append(input_guess)
-                correct_answers += 1
-            elif letter in guessed_letters and word:
+                GUESSED_LETTERS.append(input_guess)
+                CORRECT_ANSWERS += 1
+            elif letter in GUESSED_LETTERS and word:
                 print(letter, end=" ")
             else:
                 print("_", end=" ")
-        print(f"\nUsed letters:\n{guessed_letters}\n")
-    game_on = False
-    return game_on
-
+        print(f"\nUsed letters:\n{GUESSED_LETTERS}\n")
+    GAME_ON = False
+    return GAME_ON
